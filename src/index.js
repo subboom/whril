fc996ed5e84f0c9d50b4970f58c3a2d6cc6b0753
Brandon <defoe.brand@gmail.com>
2021-04-22 17:56:54 +0300

    Create InProgress page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.scss';
 
-import Home from './pages/Home';
+import InProgress from './pages/InProgress';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <InProgress />
   </React.StrictMode>,
   document.getElementById('root'),
 );
