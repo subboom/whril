23d5fc69f3e724336c537a37b15761173fad0322
Brandon <defoe.brand@gmail.com>
2021-04-22 17:23:59 +0300

    Create React display

---
+++ src/pages/Home.js
@@ -0,0 +1,191 @@
+// import MarketPlaceSection from '../containers/MarketPlaceSection';
+// import TrendingSection from '../containers/TrendingSection';
+// import Categories from '../containers/Categories';
+//
+// import { marketSections } from '../assets/data/marketplaceSections';
+
+// import { displaySection, displayCategories } from '../helpers/sectionHelpers';
+
+const Home = () => (
+  <div className="slider-container grabbing">
+    <div className="slide">
+      <h1>HomePage</h1>
+      {/* <TrendingSection display={displaySection} />
+
+      <Categories display={displayCategories} /> */}
+
+      {/* marketSections.map((section) => (
+        <section key={section.class} className={section.class}>
+          <MarketPlaceSection title={section.title} marketPlace={section.marketPlace} />
+        </section>
+      )) */}
+      {/* <footer className="__footer">
+        <div className="__center-sub-section">
+          <div className="category__options">
+            <a href="../post-ad.html" className="category">
+              <img src="../images/footer/__postad-logo.png" alt="" />
+              <h1> Post Ad </h1>
+            </a>
+            <a href="../legal/terms-and-conditions/legal.html" className="category">
+              <img src="../images/footer/__terms-logo.png" alt="" />
+              <h1>   Terms   </h1>
+            </a>
+            <a href="../legal/privacy-policy/privacy-policy.html" className="category">
+              <img src="../images/footer/__privacy-logo.png" alt="" />
+              <h1>     Privacy   </h1>
+            </a>
+            <a href="../legal/cookie-notice/cookies.html" className="category">
+              <img src="../images/footer/__cookies-logo.png" alt="" />
+              <h1>   Cookies   </h1>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <div className="slide">
+      <section className="__first-adpost-section">
+        <div className="__outer-sub-section">
+          <div className="__center-title">
+            <h2>Platform</h2>
+          </div>
+          <div className="__center-sub-section">
+            <div className="category__options">
+              <div className="category">
+                <img src="../images/social/light/__youtube-logo.png" alt="" />
+                <h1>
+                  YouTube
+                </h1>
+              </div>
+              <div className="category">
+                <img src="../images/social/light/__tiktok-logo.png" alt="" />
+                <h1>
+                  TikTok
+                </h1>
+              </div>
+              <div className="category">
+                <img src="../images/social/light/__instagram-logo.png" alt="" />
+                <h1>
+                  Instagram
+                </h1>
+              </div>
+              <div className="category">
+                <img src="../images/social/light/__twitter-logo.png" alt="" />
+                <h1>
+                  Twitter
+                </h1>
+              </div>
+              <div className="category">
+                <img src="../images/social/light/__snap-logo.png" alt="" />
+                <h1>
+                  SnapChat
+                </h1>
+              </div>
+              <div className="category">
+                <img src="../images/social/light/__fb-logo.png" alt="" />
+                <h1>
+                  Facebook
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="__second-adpost-section">
+        <div className="__outer-sub-section">
+          <div className="__center-title">
+            <h2>Price</h2>
+          </div>
+          <div className="__center-sub-section">
+            <div className="__slider">
+              <input id="number692" onInput="outputnumber692.value=addCommas(number692.value)"
+               type="range" lang="en-150" name="number-692" value="0"
+                min="10" max="1000" step="10" />
+              <p>
+                $
+                <output name="number-692" id="outputnumber692" htmlFor="number692">10</output>
+                {' '}
+                <p2>usd</p2>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="__third-adpost-section">
+        <div className="__outer-sub-section">
+          <div className="__center-title">
+            <h2>Followers</h2>
+          </div>
+          <div className="__center-sub-section">
+            <div className="__slider">
+              <input id="number691" onInput="outputnumber691.value=addCommas(number691.value)"
+               type="range" lang="en-150" name="number-691" value="0"
+                min="10000" max="1000000" step="10000" />
+              <p>
+                <output name="number-691" id="outputnumber691" htmlFor="number691">10,000</output>
+                <p2>+</p2>
+                {' '}
+                followers
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="__fourth-adpost-section">
+        <div className="__outer-sub-section">
+          <div className="__center-title">
+            <h2>Gist</h2>
+          </div>
+          <div className="__center-sub-section">
+            <textarea placeholder="Enter ad requirements here..." name="" id="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="__fifth-adpost-section">
+        <div className="__center-sub-section">
+          <button type="button" onClick="window.location.href='../marketplace.html">
+            <div className="glow-on-hover">
+              POST
+            </div>
+          </button>
+        </div>
+      </section>
+
+      <footer className="__footer">
+        <div className="__center-sub-section">
+          <div className="category__options">
+            <a href="../marketplace.html" className="category">
+              <img src="../images/footer/__marketplace-logo.png" alt="" />
+              <h1>
+                Marketplace
+              </h1>
+            </a>
+            <a href="../legal/terms-and-conditions/legal.html" className="category">
+              <img src="../images/footer/__terms-logo.png" alt="" />
+              <h1>
+                Terms
+              </h1>
+            </a>
+            <a href="../legal/privacy-policy/privacy-policy.html" className="category">
+              <img src="../images/footer/__privacy-logo.png" alt="" />
+              <h1>
+                Privacy
+              </h1>
+            </a>
+            <a href="../legal/cookie-notice/cookies.html" className="category">
+              <img src="../images/footer/__cookies-logo.png" alt="" />
+              <h1>
+                Cookies
+              </h1>
+            </a>
+          </div>
+        </div>
+      </footer> */}
+    </div>
+  </div>
+);
+
+export default Home;
